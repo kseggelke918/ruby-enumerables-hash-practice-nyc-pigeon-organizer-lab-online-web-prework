3c5ca5265ce11d3ec95d3817024c0f95d0c9cd57
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-01 20:46:42 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,5 @@
 def nyc_pigeon_organizer(data)
-  #collect each pigeon name from the hash and make it the key of the new hash
-  #have each new hash have hashes with the values of 1. color 2. gender 3. lives as keys
+  new_hash = {}
+  data.each do |trait|
+    
 end
